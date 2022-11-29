a5d68fc7f60f9eb841f54a698de9fd1a8b5013d9
Swati-Vaddoriya <infinitebitzee@gmail.com>
2022-11-29 22:48:15 +0530

    Update main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("args = " + Arrays.deepToString(args));
-
-
-
-
+        System.out.print("My first program in java");
+        print("hey github");
+    }
+    public static void print(String str){
+        System.out.println("My Name is "+str);
     }
 }
