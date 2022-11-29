6940f4c4b4435b47439f20956ea59d32c87cc71a
Swati-Vaddoriya <infinitebitzee@gmail.com>
2022-11-29 22:51:21 +0530

    added show function

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("My first program in java");
         print("hey github");
+        show("display github");
     }
     public static void print(String str){
+        System.out.println("My Name is "+str);
+    }
+    public static void show(String str){
         System.out.println("My Name is "+str);
     }
 }
